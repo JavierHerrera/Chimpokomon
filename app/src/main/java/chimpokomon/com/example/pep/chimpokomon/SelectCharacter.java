b65b9f446117e3f266c2c6cdaab2c44f42d0666e
Javier Herrera <javierherrerasanches@gmail.com>
2017-03-12 22:16:56 -0700

    first commit

---
+++ app/src/main/java/chimpokomon/com/example/pep/chimpokomon/SelectCharacter.java
@@ -0,0 +1,13 @@
+package chimpokomon.com.example.pep.chimpokomon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectCharacter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_character);
+    }
+}
