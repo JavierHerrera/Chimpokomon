f25e88928c5cc9faa1ca65694ffdcdfef29eb0d6
Javier Herrera <javierherrerasanches@gmail.com>
2017-03-16 22:56:17 -0700

    Pantalla de seleccion cuenta con cardview(beta)

--- app/src/main/java/chimpokomon/com/example/pep/chimpokomon/SelectCharacter.java
+++ app/src/main/java/chimpokomon/com/example/pep/chimpokomon/SelectCharacter.java
@@ -3,11 +3,27 @@ package chimpokomon.com.example.pep.chimpokomon;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SelectCharacter extends AppCompatActivity {
 
     public MediaPlayer mpStreetFighter;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<String> mDataset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +33,19 @@ public class SelectCharacter extends AppCompatActivity {
         //Inicia audio de streetfighter
         mpStreetFighter = MediaPlayer.create(SelectCharacter.this, R.raw.streetfightersong);
         mpStreetFighter.start();
+
+        //RecyclerView
+        mDataset = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            mDataset.add("New Title # " + i);
+        }
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MainAdapter(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 }
